cfdcb985ffa1ea02b06bbe780df5df1ef1066d54
MbonuCharles <saintabuchi@gmail.com>
2022-08-27 12:50:25 -0700

    my first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import TaskManager from "./components/taskManager/TaskManager";
+
+function App() {
+  return (
+    <div className="App">
+      <TaskManager/>
+
+      
+    </div>
+  );
+}
+
+export default App;
